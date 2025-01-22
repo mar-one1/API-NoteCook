63a90e7b6604eb284fee3c4192893723a4e3d77b
marwane <marwane.rays@gmail.com>
2025-01-22 13:43:56 +0100

    fix2222

--- Api/Router/user_router_controllers.js
+++ Api/Router/user_router_controllers.js
@@ -1,7 +1,12 @@
-const express = require("express");
-const router = express.Router();
-const bookController = require('../Controllers/user_controllers');
+const express = require("express")
+const router = express.Router()
 
-router.all("*", bookController.handler);
+const bookController = require('../Controllers/user_controllers')
 
-module.exports = router;
+router.get("/", bookController.getAll)
+/*router.get("/:id", bookController.getById)
+router.post("/", bookController.create)
+router.put("/:id", bookController.updateById)
+router.delete("/:id", bookController.deleteById)
+*/
+module.exports = router
