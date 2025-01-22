4dde7870fcade12c13c9f7e4837c24598f55b3c1
marwane <marwane.rays@gmail.com>
2025-01-22 12:58:34 +0100

    add postgres db

---
+++ Api/Router/user_router_controllers.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+
+const bookController = require('../Controllers/user_controllers')
+
+router.get("/", bookController.getAll)
+router.get("/:id", bookController.getById)
+router.post("/", bookController.create)
+router.put("/:id", bookController.updateById)
+router.delete("/:id", bookController.deleteById)
+
+module.exports = router
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.static('public'));
 const swaggerSetup = require('./Api/swagger');
 const messageModel = require('./Api/Model/chat'); // Replace with your actual message model
 const cors = require('cors');
+require('dotenv').config()
 
 
 const { deleteUnusedImages } = require('./Api/Router/ImageHelper');
@@ -29,6 +30,9 @@ const favRouter = require('./Api/Router/fav_user_recipe_Router');
 const recipeModelRouter = require('./Api/Repo/recipeModelRouter'); 
 const categoryModelRouter = require('./Api/Router/category_Router');
 
+//vercel
+const bookRouter = require('./Api/Router/user_router_controllers')
+app.use("/api/v1/books", bookRouter)
 
 
 app.delete('/cleanup-images', async (req, res) => {
