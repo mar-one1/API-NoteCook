dd8d271f7f0d434bc47e6c398176f8731c8ece11
marwane <marwane.rays@gmail.com>
2025-02-05 09:50:09 +0100

    user sql router

--- Api/Router/usersRouter.js
+++ Api/Router/usersRouter.js
@@ -1,16 +1,54 @@
-// userRouter.js
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../Model/User"); // Assuming userModel.js is inside a Model directory
-const bodyParser = require("body-parser");
-const multer = require("multer");
-const fs = require("fs");
-const { validationResult } = require("express-validator");
-const validateUser = require("../validators/validateUser"); // Your validation logic
-const upload = multer({ dest: "uploads/" }); // Change this if you want a different folder for image upload
+const User = require('../Model/User');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
+const fs = require('fs');
+router.use(express.json()); 
+router.use(express.urlencoded({ extended: true }));
+const UserRepository  = require('../Repo/UserRepository'); // Replace with the actual path
+const { body, validationResult } = require('express-validator');
+const validateUser = require('../validators/validateUser');
+
+
+// Get a user by ID
+/*router.get('/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    await UserRepository.getUserById(userId, (err, user) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      if (!user) {
+        return res.status(406).json({ error: 'User not found' });
+      }
+      res.json(user);
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});*/
+
+
+//router.use(bodyParser.json());
+const storage = multer.diskStorage({
+  destination: './public/uploads/', // Destination directory
+  filename: function (req, file, cb) {
+      // Define a custom file name (you can modify this logic)
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, uniqueSuffix + '-' + file.originalname);
+  } 
+//const upload = multer({ storage: storage });
+});
+const upload = multer({ dest: 'uploads/', storage: storage})
 
 // Create a new user
-router.post("/", validateUser.validateUserRegistration, async (req, res) => {
+// Create a new user
+router.post('/', validateUser.validateUserRegistration, async (req, res) => {
+
+  // Extract user data from the request body
   const {
     username,
     firstname,
@@ -25,89 +63,149 @@ router.post("/", validateUser.validateUserRegistration, async (req, res) => {
     url,
   } = req.body;
 
+  // Check for validation errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  try {
-    const newUser = await User.createUser(
-      username,
-      firstname,
-      lastname,
-      birthday,
-      email,
-      phoneNumber,
-      icon,
-      password,
-      grade,
-      status,
-      url
-    );
+  // Create the user in the database
+  User.createUser(
+  username,
+  firstname,
+  lastname,
+  birthday,
+  email,
+  phoneNumber,
+  icon,
+  password,
+  grade,
+  status,
+  url,
+  (err, newUser) => {
+    
+    if (err) {
+      if (err.message === 'User already exists') {
+        return res.status(409).json({ error: 'User already exists' });
+      }
+      else {
+      return res.status(500).json({ error: err.message });
+      }
+    }
     res.status(201).json(newUser);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
   }
+  );
+});
+
+router.delete('/delete/:path', (req, res) => {
+  const pathimage = req.params.path;
+  console.log('path for delete '+pathimage);
+  User.deleteimage(pathimage,(err, validite) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.status(201).json(validite);
+  });
+});
+
+router.post('/upload/:username', upload.single('image'),async  (req, res) => {
+  const username = req.params.username;
+  console.log(req.body);
+  console.log(req.file);
+  if (!req.file) {
+      return res.status(400).send('No file uploaded.');
+  }
+  // Process the uploaded file
+  const fileName = req.file.filename;
+  const imageUrl = encodeURIComponent(fileName);
+  console.log(username);
+
+  User.UpdateUserImage(username,imageUrl,(err, validite) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  // If the user doesn't exist, add them to the database
+    res.status(201).json(validite);
+  });
 });
 
 // Get a user by ID
-router.get("/:id", async (req, res) => {
-  try {
-    const user = await User.getUserById(req.params.id);
+router.get('/:id', (req, res) => {
+  const userId = req.params.id;
+
+  User.getUserById(userId, (err, user) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(406).json({ error: 'User not found' });
     }
     res.json(user);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  });
 });
 
-// Get a user by Username
-router.get("/filtre/:username", async (req, res) => {
-  try {
-    const user = await User.getUserByUsername(req.params.username);
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// Update user image by username
-router.post("/upload/:username", upload.single("image"), async (req, res) => {
+// Get a user by USERNAME
+router.get('/filtre/:username', (req, res) => {
   const username = req.params.username;
-  if (!req.file) {
-    return res.status(400).send("No file uploaded.");
-  }
-
-  const imageUrl = encodeURIComponent(req.file.filename);
-
-  try {
-    const updatedUser = await User.updateUserImage(username, imageUrl);
-    if (!updatedUser) {
-      return res.status(404).json({ error: "User not found" });
+console.log(username)
+  User.getUserByUsername(username, (err, user) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
     }
-    res.status(201).json(updatedUser);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    if (!user) {
+      return res.status(406).json({ error: 'User not found' });
+    } 
+    res.json(user);
+  });
 });
 
-// Get all users
-router.get("/", async (req, res) => {
-  try {
-    const users = await User.getAllUsers();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+// Get a user by image user
+router.get('/image/:username', (req, res) => {
+  const username = req.params.username;
+console.log("username for image is : " +username)
+  User.getUserImage(username, (err, imageByte) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (!imageByte) {
+      return res.status(406).json({ error: 'image not found' });
+    } 
+    console.log(imageByte);
+    res.json(imageByte);
+  });
 });
+
+// get All Users
+router.get('/', (req, res) => {
+  User.getAllUsers((err, users) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(users);
+  });
+});
+
+// Add more user routes as needed
+
+router.put('/image/:username', (req, res) => {
+  const username = req.params.username;
+  const {url} =req.body;
+  const imageurl = url;
+  console.log(req.body);
+  console.log(username);
+  User.UpdateUserImage(username,imageurl,(err,path) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (!path) {
+      return res.status(406).json({ error: 'User not found or not updated!!' });
+    }
+    res.json(path);
+  });
+})
 
 // Update a user by ID
-router.put("/:id", validateUser.validateUserUpdate, async (req, res) => {
+router.put('/:id', validateUser.validateUserUpdate, (req, res) => {
   const userId = req.params.id;
   const {
     username,
@@ -122,34 +220,85 @@ router.put("/:id", validateUser.validateUserUpdate, async (req, res) => {
     status,
     url,
   } = req.body;
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  try {
-    const updatedUser = await User.updateUser(userId, { username, firstname, lastname, birthday, email, phoneNumber, icon, password, grade, status, url });
+   // Check for validation errors
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+  User.updateUser(userId,username,firstname,lastname,birthday,email,phoneNumber,icon,password,grade,status,url, (err, updatedUser) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
     if (!updatedUser) {
-      return res.status(404).json({ error: "User not found or not updated" });
+      return res.status(406).json({ error: 'User not found or not updated' });
     }
     res.json(updatedUser);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  });
 });
 
-// Delete a user by ID
-router.delete("/:id", async (req, res) => {
-  try {
-    const deleted = await User.deleteUser(req.params.id);
-    if (!deleted) {
-      return res.status(404).json({ error: "User not found" });
+// Update a user by USERNAME
+router.put('/filtre/:username',  validateUser.validateUserUpdate ,async (req, res) => {
+  const username = req.params.username;
+  console.log(username);
+  const {
+    firstname,
+    lastname,
+    birthday,
+    email,
+    phoneNumber,
+    icon,
+    password,
+    grade,
+    status,
+    url,
+  } = req.body;
+   // Check for validation errors
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+  User.updateUserByUsername(username,firstname,lastname,birthday,email,phoneNumber,icon,password,grade,status,url, (err, updatedUser) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
     }
-    res.status(204).send();
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    if (!updatedUser) {
+      return res.status(406).json({ error: 'User not found or not updated' });
+    }
+    res.json(updatedUser);
+  });
 });
+
+
+
+// Delete a user by ID
+router.delete('/:id', validateUser.validateUserDelete, (req, res) => {
+  const userId = req.params.id;
+   // Check for validation errors
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+  User.deleteUser(userId, (err, deleted) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (!deleted) {
+      return res.status(406).json({ error: 'User not found or not deleted' });
+    }
+    res.json({ message: 'User deleted successfully' });
+  });
+  // Implement delete logic here using the User model
+});
+
+// Add more user routes with parameters as needed
+
+// Define a route for the root URL
+router.get('/', (req, res) => {
+  res.send('Hello from the router User!');
+});
+
+
+module.exports = router;
+
 
 module.exports = router;
